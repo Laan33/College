a0bab70560b88746c5bc7ab0217bd1f1a6491988
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-03-02 14:57:50 +0000

    Showing Stephen git demo + assignment 7 update

--- Year 2/CT255 Next Gen Technology's - 2D Games/Assignment 7 Cathal/ConwaysApplication.java
+++ Year 2/CT255 Next Gen Technology's - 2D Games/Assignment 7 Cathal/ConwaysApplication.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.image.*;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import java.awt.event.MouseEvent;
 import javax.swing.event.MouseInputListener;
@@ -10,9 +12,14 @@ public class ConwaysApplication extends JFrame implements MouseInputListener, Ru
     final int cells = 40; //how many cells we wants
     final int pixel = 20; //size of each cell in pixels
     private boolean[][] gamePointsBool = new boolean[cells][cells]; //array of boolean cells 
+    //private boolean[][][] gamePointsBool = new boolean[cells][cells][2]; //array of boolean cells 
+    private int currentlyDisplayed = 0;
+
+    
 
     private static final Dimension WindowSize = new Dimension(800, 800);    
     private BufferStrategy strategy;
+    boolean isGamePlaying = false;
     
     public ConwaysApplication() {
         addMouseListener(this);
@@ -26,17 +33,87 @@ public class ConwaysApplication extends JFrame implements MouseInputListener, Ru
         setBounds(x, y, WindowSize.width, WindowSize.height);
         setVisible(true);
         this.setTitle("Conways Game of Life");
+        
+        
 
         createBufferStrategy(2);
         strategy = getBufferStrategy();
+
+        JButton buttonStart = new JButton("Start");
+        buttonStart.setBounds(0, 0, 100, 30);
+        buttonStart.addActionListener(e -> {
+            isGamePlaying = true; // set the game state to playing
+            buttonStart.setVisible(false); // hide the button
+        });
+
+        JButton buttonRandom = new JButton("Random");
+        buttonStart.setBounds(0, 0, 100, 30);
+        buttonStart.addActionListener(e -> {
+            isGamePlaying = true; // set the game state to playing
+            buttonStart.setVisible(false); // hide the button
+        });
+
+        if (!isGamePlaying) { // if the game is not playing
+            // create a new button and add it to the top left corner of the JFrame
+            this.add(buttonStart);
+            this.add(buttonRandom);
+        }
     }
+
+    public void random(boolean[][] gamePointsBool) {
+        
+    }
+
+    public void conwayNeighbourChecker(boolean[][][] gamePointsBool, int currentlyDisplayed) {
+        int[][][] liveNeighbours = new int[40][40][2];
+        for (int x=0;x<40;x++) {
+            for (int y=0;y<40;y++) {
+                // count the live neighbours of cell [x][y][0]
+                for (int xx=-1;xx<=1;xx++) {
+                    for (int yy=-1;yy<=1;yy++) {
+                        //if (gamePointsBool[x + xx][y + yy][currentlyDisplayed] != true || gamePointsBool[xx][yy][]!=0) {
+                        if (gamePointsBool[(x + xx) % 40][(y + yy) % 40][currentlyDisplayed] != true) {
+                            liveNeighbours[x][y][(currentlyDisplayed + 1) % 2]++;
+                        // check cell [x+xx][y+yy][0]
+                        // but.. what if x+xx==-1, etc. ?
+                        }
+                    }
+                }
+                int count = liveNeighbours[x][y][(currentlyDisplayed + 1) % 2];
+                if(count < 2 || count > 3) {
+                    gamePointsBool[x][y][(currentlyDisplayed + 1) % 2] = false;
+                } else if(count == 3) { //if there is 3 neighbours this cells will be made alive regardless if previously dead
+                    gamePointsBool[x][y][(currentlyDisplayed + 1) % 2] = true;
+                } else if(count == 2 && gamePointsBool[x][y][(currentlyDisplayed + 1) % 2]) {
+                    gamePointsBool[x][y][(currentlyDisplayed + 1) % 2] = true; //if there is two and cell is alive - keep alive
+                }
+            }      
+        }
+
+
+        
+
+
+    }
+
 
     @Override
     public void paint(Graphics g) {
+        
+        try{
+        if(!isGamePlaying) {
+                    
+        } else {
+            //conwayNeighbourChecker(gamePointsBool, currentlyDisplayed);
+        }
+        
         g = strategy.getDrawGraphics(); //redirecting drawing calls to offscreen buffer
+
+        
 
         for(int i = 0; i < cells; i++) { //for all the cells on each row
             for(int j = 0; j < cells; j++) { //and all the cells in a column  
+                //if (gamePointsBool[i][j][(currentlyDisplayed + 1) % 2]) { //if the cell is true, set it to white
                 if (gamePointsBool[i][j]) { //if the cell is true, set it to white
                     g.setColor(Color.WHITE);                     
                 }
@@ -46,7 +123,14 @@ public class ConwaysApplication extends JFrame implements MouseInputListener, Ru
                 g.fillRect(i * pixel, j * pixel, pixel, pixel); //paint the cell
             }
         }
-        strategy.show(); //flip the buffers 
+        
+        //ConwaysApplication.add(button);
+        strategy.show(); //flip the buffers
+        currentlyDisplayed = (currentlyDisplayed + 1) % 2;
+        System.out.println("Currently displayed: " + currentlyDisplayed);
+    } catch(Exception e) {
+        System.out.println("oops");
+    } 
     }
 
     @Override
@@ -71,9 +155,10 @@ public class ConwaysApplication extends JFrame implements MouseInputListener, Ru
 
     public void mouseClicked(MouseEvent e) {
         int x = e.getX() / pixel; //getting the x of the cell  
-        int y = e.getY() / pixel; //gettign the y of the cell
+        int y = e.getY() / pixel; //getting the y of the cell
 
-        gamePointsBool[x][y] = !gamePointsBool[x][y]; //change the cell to the inverse of itself
+        //gamePointsBool[x][y][currentlyDisplayed] = !gamePointsBool[x][y][currentlyDisplayed]; //change the cell to the inverse of itself
+        gamePointsBool[x][y]= !gamePointsBool[x][y]; //change the cell to the inverse of itself
         
         repaint();
     }
