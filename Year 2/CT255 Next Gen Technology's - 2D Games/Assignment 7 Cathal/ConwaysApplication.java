71f375c6a01e48be3519220d05e262300c3e003e
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-03-03 18:05:37 +0000

    git commit

--- Year 2/CT255 Next Gen Technology's - 2D Games/Assignment 7 Cathal/ConwaysApplication.java
+++ Year 2/CT255 Next Gen Technology's - 2D Games/Assignment 7 Cathal/ConwaysApplication.java
@@ -11,10 +11,7 @@ public class ConwaysApplication extends JFrame implements MouseInputListener, Ru
 
     final int cells = 40; // how many cells we wants
     final int pixel = 20; // size of each cell in pixels
-    // private boolean[][] gamePointsBool = new boolean[cells][cells]; //array of
-    // boolean cells
     private boolean[][][] gamePointsBool = new boolean[cells][cells][2]; // array of boolean cells
-    // private int currentlyDisplayed = 0;
 
     private static final Dimension WindowSize = new Dimension(800, 800);
     private BufferStrategy strategy;
@@ -35,17 +32,16 @@ public class ConwaysApplication extends JFrame implements MouseInputListener, Ru
 
         addMouseListener(this);
 
-        for (int i = 0; i < cells; i++) {
+        for (int i = 0; i < cells; i++) { // initialising cells
             for (int j = 0; j < cells; j++) {
                 gamePointsBool[i][j][0] = false;
             }
         }
 
-        isGraphicsInstantiated = true;
+        isGraphicsInstantiated = true; // everything is instantiated
 
         Thread t = new Thread(this);
         t.start();
-        
 
     }
 
@@ -54,82 +50,60 @@ public class ConwaysApplication extends JFrame implements MouseInputListener, Ru
             for (int y = 0; y < 40; y++) {
                 Random rand = new Random();
                 int randomNum = rand.nextInt(2); // generate either 0 or 1 randomly
-                gamePointsBool[x][y][0] = (randomNum == 1);
-                gamePointsBool[x][y][1] = (randomNum == 1);
+                gamePointsBool[x][y][0] = (randomNum == 1); //if one then true, 0 false
             }
         }
     }
-
-    
 
     public void paint(Graphics g) {
-        if(isGraphicsInstantiated) {
-            g = strategy.getDrawGraphics(); //redirecting drawing calls to offscreen buffer
-           
-        g.setColor(Color.BLACK);   
-        g.fillRect(0, 0, 800, 800); 
+        if (isGraphicsInstantiated) {
+            g = strategy.getDrawGraphics(); // redirecting drawing calls to offscreen buffer
 
-        /*
-        if(!isGamePlaying) {
-            g.setColor(Color.WHITE);
-            g.fillRect(10, 35, 200, 40); 
-            g.setColor(Color.GREEN); 
-            g.fillRect(15, 40, 90, 30); 
-            g.fillRect(115, 40, 90, 30); 
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, 800, 800);
 
-            g.setColor(Color.BLUE);
-            g.setFont(new Font("Arial", Font.BOLD, 17));
-            g.drawString("START", 25, 60);
-            g.drawString("RANDOM", 120, 60);
-        
-        } */
-              
-        for(int i = 0; i < cells; i++) { //for all the cells on each row
-            for(int j = 0; j < cells; j++) { //and all the cells in a column  
-                //if (gamePointsBool[i][j][(currentlyDisplayed + 1) % 2]) { //if the cell is true, set it to white
-                if (gamePointsBool[i][j][0]) { //if the cell is true, set it to white
-                    g.setColor(Color.WHITE);                     
+            for (int i = 0; i < cells; i++) { // for all the cells on each row
+                for (int j = 0; j < cells; j++) { // and all the cells in a column
+                    if (gamePointsBool[i][j][0]) { // if the cell is true, set it to white
+                        g.setColor(Color.WHITE);
+                    } else { // else set the cell to black
+                        g.setColor(Color.BLACK);
+                    }
+                    g.fillRect(i * pixel, j * pixel, pixel, pixel); // paint the cell
                 }
-                else { //else set the cell to black
-                    g.setColor(Color.BLACK);                    
-                }
-                g.fillRect(i * pixel, j * pixel, pixel, pixel); //paint the cell
             }
-        }
-        
-        if(!isGamePlaying) {
-            g.setColor(Color.WHITE);
-            g.fillRect(10, 35, 200, 40); 
-            g.setColor(Color.GREEN); 
-            g.fillRect(15, 40, 90, 30); 
-            g.fillRect(115, 40, 90, 30); 
 
-            g.setColor(Color.BLUE);
-            g.setFont(new Font("Arial", Font.BOLD, 17));
-            g.drawString("START", 25, 60);
-            g.drawString("RANDOM", 120, 60);
-        
+            if (!isGamePlaying) { //painting buttons
+                g.setColor(Color.WHITE);
+                g.fillRect(10, 35, 200, 40);
+                g.setColor(Color.GREEN);
+                g.fillRect(15, 40, 90, 30);
+                g.fillRect(115, 40, 90, 30);
+
+                g.setColor(Color.BLUE);
+                g.setFont(new Font("Arial", Font.BOLD, 17));
+                g.drawString("START", 25, 60);
+                g.drawString("RANDOM", 120, 60);
+
+            }
+
+            strategy.show(); // flip the buffers
+
         }
-        
-        strategy.show(); //flip the buffers
-        
-    } 
     }
 
-
-    public void run() {        
+    public void run() {
         while (1 == 1) {
             try {
-                Thread.sleep(500);
+                Thread.sleep(400); //repeats every 0.4 seconds
 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
             if (isGamePlaying) {
-                
                 for (int i = 0; i < cells; i++) {
-                    for (int j = 0; j < cells; j++) {
+                    for (int j = 0; j < cells; j++) { //sets the back array to false
                         gamePointsBool[i][j][1] = false;
                     }
                 }
@@ -139,8 +113,8 @@ public class ConwaysApplication extends JFrame implements MouseInputListener, Ru
                         // count the live neighbours of cell [x][y][0]
                         for (int xx = -1; xx <= 1; xx++) {
                             for (int yy = -1; yy <= 1; yy++) {
-                                //if x+xx==-1, etc. we use modulus to loop back
-                                //java % 39 on -1 isn't 39, it's still -1..., thats why we use .floorMod
+                                // if x+xx==-1, etc. we use modulus to loop back
+                                // java % 39 on -1 isn't 39, it's still -1..., thats why we use .floorMod
                                 int X = Math.floorMod(x + xx, 39);
                                 int Y = Math.floorMod(y + yy, 39);
 
@@ -152,24 +126,23 @@ public class ConwaysApplication extends JFrame implements MouseInputListener, Ru
 
                         if (neighbourCount < 2 && gamePointsBool[x][y][0]) {
                             gamePointsBool[x][y][1] = false;
-                        } else if(neighbourCount > 3 && gamePointsBool[x][y][0]) {
+                        } else if (neighbourCount > 3 && gamePointsBool[x][y][0]) {
                             gamePointsBool[x][y][1] = false;
-                        } else if ((neighbourCount == 3 || neighbourCount == 2) && gamePointsBool[x][y][0]) { // if there is 3 neighbours this cells will be kept alive / made alive
+                        } else if ((neighbourCount == 3 || neighbourCount == 2) && gamePointsBool[x][y][0]) { 
+                            // if there is 3 neighbours this cells will be kept alive / made alive
                             gamePointsBool[x][y][1] = true;
                         } else if (neighbourCount == 3 && !gamePointsBool[x][y][0]) {
                             gamePointsBool[x][y][1] = true; // if there is two and cell is alive - keep alive
                         }
                     }
-
                 }
 
                 for (int i = 0; i < cells; i++) {
-                    for (int j = 0; j < cells; j++) {
+                    for (int j = 0; j < cells; j++) { //setting front cells to the new calculated cells
                         gamePointsBool[i][j][0] = gamePointsBool[i][j][1];
                     }
                 }
             }
-
             this.repaint();
         }
 
@@ -179,11 +152,11 @@ public class ConwaysApplication extends JFrame implements MouseInputListener, Ru
         int x = e.getX() / pixel; // getting the x of the cell
         int y = e.getY() / pixel; // getting the y of the cell
 
-        if (!isGamePlaying && e.getX() < 110 && e.getY() < 70) {
-            isGamePlaying = true; //start game
+        if (!isGamePlaying && e.getX() < 110 && e.getY() < 70) { //start region
+            isGamePlaying = true; // start game
         }
-        if (!isGamePlaying && e.getX() > 110 && e.getX() < 210 && e.getY() < 70) {
-            random(gamePointsBool); //randomise the cells
+        if (!isGamePlaying && e.getX() > 110 && e.getX() < 210 && e.getY() < 70) { //random region
+            random(gamePointsBool); // randomise the cells
         }
         gamePointsBool[x][y][0] = !gamePointsBool[x][y][0]; // change the cell to the inverse of itself
 
