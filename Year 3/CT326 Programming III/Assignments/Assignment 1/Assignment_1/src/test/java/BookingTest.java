86bd4151cb13aac7ac8b4ea54fe804ad1eaaa9fa
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-09-19 16:45:10 +0100

    programming TDD booking

--- Year 3/CT326 Programming III/Assignments/Assignment 1/Assignment_1/src/test/java/BookingTest.java
+++ Year 3/CT326 Programming III/Assignments/Assignment 1/Assignment_1/src/test/java/BookingTest.java
@@ -95,61 +95,6 @@ public class BookingTest {
         }
     }
 
-
-
 }
 
-class AccountTest {
-    @BeforeEach
-    void setUp() throws Exception {
-    }
-    @Test
-    void testMakeAValidDeposit() {
-        Account account;
-        try {
-            account = new Account(123, 0);
-            account.makeDeposit(500.0);
-            assertTrue(account.getBalance() == 500.0);
-            account.makeDeposit(10.0);
-            assertTrue(account.getBalance() == 510.0);
-        } catch (NegativeBalanceException e) {
-            e.printStackTrace();
-        }
-    }
-    @Test
-    void testMakeInvalidDeposit_negativeAmount() {
-        Account account;
-        try {
-            account = new Account(123, 0);
-            double prevBalance = account.getBalance();
-            account.makeDeposit(-500.0);
-            assertTrue(account.getBalance() == prevBalance);
-        } catch (NegativeBalanceException e) {
-            e.printStackTrace();
-        }
-    }
-    @Test
-    void testValidWithdrawal() {
-        try {
-            Account account = new Account(123, 0);
-            account.makeDeposit(50);
-            account.makeWithdrawal(10);
-            assertTrue(account.getBalance() == 40);
-        } catch (NegativeBalanceException e ) {
-            e.printStackTrace();
-        } catch (InsufficientFundsException e) {
-            e.printStackTrace();
-        }
-    }
-    @Test
-    void testInvalidWithdrawal_insufficientFunds() {
-        try {
-            Account account = new Account(123, 0);
-            assertThrows(InsufficientFundsException.class, () -> {
-                account.makeWithdrawal(300);
-            });
-        } catch (NegativeBalanceException e) {
-            e.printStackTrace();
-        }
-    }
-}
+
