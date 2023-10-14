44652533ff5a579e0a01a0fdb063d3aada4fe7de
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-10-14 18:09:42 +0100

    Assignment 3 write & read object

--- Year 3/CT326 Programming III/Assignments/Assignment 3/Assignment_3/src/main/java/Achievement.java
+++ Year 3/CT326 Programming III/Assignments/Assignment 3/Assignment_3/src/main/java/Achievement.java
@@ -2,15 +2,28 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 public class Achievement implements Serializable {
-    String achievementName;
-    String description;
-    LocalDate dateOfAward;
+    private String achievementName;
+    private String description;
+    private LocalDate dateOfAward;
 
 
     public Achievement(String achievementName, String description, LocalDate dateOfAward) {
         this.achievementName = achievementName;
         this.description = description;
         this.dateOfAward = dateOfAward;
-
     }
+
+    public String getAchievementName() {
+        return achievementName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDateOfAward() {
+        return dateOfAward;
+    }
+
+
 }
--- Year 3/CT326 Programming III/Assignments/Assignment 3/Assignment_3/src/main/java/Player.java
+++ Year 3/CT326 Programming III/Assignments/Assignment 3/Assignment_3/src/main/java/Player.java
@@ -5,54 +5,68 @@ String id, String username, Country country, LocalDate joinDate,
 List achievements
  */
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
  */
 public class Player implements Serializable {
-    String id;
-    String username;
-    Country country;
-    LocalDate joinDate;
+    private final String id;
+    private String username;
+    private LocalDate joinDate;
     transient List<Achievement> achievements;
 
 
     public Player(String id, String username, Country country, LocalDate joinDate, List<Achievement> achievements) {
         this.id = id;
         this.username = username;
-        this.country = country;
         this.joinDate = joinDate;
         this.achievements = achievements;
     }
 
-    public void writeObject(ObjectOutputStream os) throws IOException {
+    public String getId() {
+        return id;
+    }
+
+    @Serial
+    private void writeObject(ObjectOutputStream os) throws IOException {
         os.defaultWriteObject();
 
-
-    }
-
-    private void readObject(ObjectInputStream is) throws IOException {
-        try {
-            is.defaultReadObject();
-
-            /*LocalDate regDate = (LocalDate) is.readObject();
-            String reg = (String) is.readObject();
-
-            this.setRegistration(new Registration(regDate, reg));*/
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        FileWriter writer = new FileWriter("achievements.csv");
+        for (Achievement achievement : achievements) {
+            writer.write(id + "," + achievement.getAchievementName() + "," + achievement.getDescription() + "\n");
         }
+        writer.close();
     }
+
+
+
+    @Serial
+    private void readObject(ObjectInputStream is) throws IOException, ClassNotFoundException {
+        is.defaultReadObject(); // Read in all non-transient, non-static fields
+
+        // Read in the achievements field from the achievements.csv file
+        achievements = new ArrayList<>();
+        String playerId = getId();
+        Scanner scanner = new Scanner(new File("achievements.csv"));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(",");
+            if (parts.length == 4 && parts[0].equals(playerId)) {
+                String achievementName = parts[1];
+                String description = parts[2];
+                LocalDate dateOfAward = LocalDate.parse(parts[3]);
+                Achievement achievement = new Achievement(achievementName, description, dateOfAward);
+                achievements.add(achievement);
+            }
+        }
+        scanner.close();
+    }
+
 
 
 }
