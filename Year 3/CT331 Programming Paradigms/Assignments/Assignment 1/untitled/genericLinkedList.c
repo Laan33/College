ce0f5d8fa242130683ef123f0c9512fdd76a137a
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-10-19 23:15:49 +0100

    Question 3

---
+++ Year 3/CT331 Programming Paradigms/Assignments/Assignment 1/untitled/genericLinkedList.c
@@ -0,0 +1,5 @@
+//
+// Created by catha on 19/10/2023.
+//
+
+#include "genericLinkedList.h"
---
+++ Year 3/CT331 Programming Paradigms/Assignments/Assignment 1/untitled/genericLinkedList.h
@@ -0,0 +1,12 @@
+//
+// Created by catha on 19/10/2023.
+//
+
+#ifndef UNTITLED_GENERICLINKEDLIST_H
+#define UNTITLED_GENERICLINKEDLIST_H
+
+#endif //UNTITLED_GENERICLINKEDLIST_H
+
+
+//extend the linked list to accept any data type ( Hint: Use
+//void* to create a pointer to any data type.)
