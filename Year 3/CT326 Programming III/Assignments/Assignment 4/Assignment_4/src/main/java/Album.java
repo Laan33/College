e8b8ff1fdc2252f7ce7df275ed0f55d27bfa133c
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-10-29 17:37:57 +0000

    Programming album GUI

---
+++ Year 3/CT326 Programming III/Assignments/Assignment 4/Assignment_4/src/main/java/Album.java
@@ -0,0 +1,47 @@
+
+
+/*
+Details of the albums (Artist, album name, cover image file, track listing file) are contained in
+‘music_library.txt’
+ */
+public class Album {
+    private String artist;
+    private String albumName;
+    private String coverImageFile;
+    private String trackListingFile;
+
+    public Album(String artist, String albumName, String coverImageFile, String trackListingFile) {
+        this.artist = artist;
+        this.albumName = albumName;
+        this.coverImageFile = coverImageFile;
+        this.trackListingFile = trackListingFile;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public String getCoverImageFile() {
+        return coverImageFile;
+    }
+
+    public String getTrackListingFile() {
+        return trackListingFile;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "artist='" + artist + '\'' +
+                ", albumName='" + albumName + '\'' +
+                ", coverImageFile='" + coverImageFile + '\'' +
+                ", trackListingFile='" + trackListingFile + '\'' +
+                '}';
+    }
+
+
+}
