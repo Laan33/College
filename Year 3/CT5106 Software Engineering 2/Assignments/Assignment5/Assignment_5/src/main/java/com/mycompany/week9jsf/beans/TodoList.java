10791b47c63a3caf0766f8ef7abbcfbeb53c5036
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-11-03 18:30:12 +0000

    Assignment 5 - Software - Table headers

--- Year 3/CT5106 Software Engineering 2/Assignments/Assignment5/Assignment_5/src/main/java/com/mycompany/week9jsf/beans/TodoList.java
+++ Year 3/CT5106 Software Engineering 2/Assignments/Assignment5/Assignment_5/src/main/java/com/mycompany/week9jsf/beans/TodoList.java
@@ -4,42 +4,64 @@
  */
 package com.mycompany.week9jsf.beans;
 
-
 import com.mycompany.week9jsf.data.Todo;
 import com.mycompany.week9jsf.services.TodoFacade;
 import jakarta.annotation.PostConstruct;
 import jakarta.ejb.EJB;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
-
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author 0063190S
- */
 @Named("todoListBean")
 @RequestScoped
-public class TodoList implements Serializable
-{
+public class TodoList implements Serializable {
 
     @EJB
     private TodoFacade todoFacade;
 
     private List<Todo> todoList;
-    
+
+    // Define the column model for the table
+    private List<ColumnModel> columnModel;
+
     @PostConstruct
-    public void postConstruct()
-    {
-        System.out.println("calling todo facade");
+    public void postConstruct() {
+        // Initialize the columnModel with headers and property names
+        columnModel = new ArrayList<>();
+        columnModel.add(new ColumnModel("Category", "category"));
+        columnModel.add(new ColumnModel("Description", "description"));
+        columnModel.add(new ColumnModel("Priority", "priority"));
+
+        // Retrieve the todo list
         todoList = todoFacade.findAll();
-        System.out.println("num todos = " + todoList.size());
     }
-    
-       public List<Todo> getTodoList() {
-           System.out.println("calling getTodoList");
-           System.out.println("Size of getTodoList   -   " + todoList.size() );
+
+    public List<Todo> getTodoList() {
         return todoList;
+    }
+
+    public List<ColumnModel> getColumnModel() {
+        return columnModel;
+    }
+
+    // ColumnModel class to represent headers and properties
+    public class ColumnModel {
+        private String header;
+        private String property;
+
+        public ColumnModel(String header, String property) {
+            this.header = header;
+            this.property = property;
+        }
+
+        public String getHeader() {
+            return header;
+        }
+
+        public String getProperty() {
+            return property;
+        }
     }
 }
