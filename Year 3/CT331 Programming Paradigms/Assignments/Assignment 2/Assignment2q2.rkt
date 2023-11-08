c6cc7b51d8669576d9a6df99c03b401ba571f91d
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-11-08 20:15:45 +0000

    List assignment 2

--- Year 3/CT331 Programming Paradigms/Assignments/Assignment 2/Assignment2q2.rkt
+++ Year 3/CT331 Programming Paradigms/Assignments/Assignment 2/Assignment2q2.rkt
@@ -6,40 +6,94 @@
 ;;inserts the element at the beginning of the list. You can assume only valid
 ;;elements and lists.
 ;;e.g., if the function is called ins_beg:
-;;a. (ins_beg ‘a ‘(b c d)) returns the list (a b c d)
-;;b. (ins_beg ‘(a b) ‘(b c d)) returns the list ((a b) b c d)
+;;a. (ins_beg 'a '(b c d)) returns the list (a b c d)
+;;b. (ins_beg '(a b) '(b c d)) returns the list ((a b) b c d)
 
 
 (provide ins_beg)
 (provide ins_end)
+(provide cout_top_level)
+(provide count_instances)
+(provide tail_count_instances)
+(provide tail_count_instances_helper)
+(provide count_instances_deep)
 
 
+;; ins_beg: Scheme function that takes an element and a list and returns a new list with the element added to the beginning of the original list.
 (define (ins_beg el lst)
-  (cons el lst))
+  (cons el lst)) ;;creating new list containing the new element at the start using cons
 
-
-;;B. Write a Scheme function named ins_end which, when passed an element and a
-;;list, inserts the element at the end of the list. You can assume only valid elements
-;;and lists.
-;;e.g., if the function is called ins_end:
-;;a. (ins_end ‘a ‘(b c d)) returns the list (b c d a)
-;;b. (ins_end ‘(a b) ‘(b c d)) returns the list (b c d (a b))
-
-
-
+;; ins_end: Scheme function that takes an element and a list and returns a new list with the element added to the end of the original list.
 (define (ins_end el lst)
-  (cons lst el))
+  (append lst (cons el empty))) ;;Assuming lst is a list, and appending a new list (made using cons) with el to the end of lst
 
-;;C. Write a Scheme function named cout_top_level that counts the number of top-
-;;levels
-;;items in a list, i.e., you don’t need to include items in a sub list.
+;; cout_top_level: Scheme function that takes a list and returns the number of top-level items in the list (i.e., items that are not in a sublist).
+(define (cout_top_level lst)
+  (if (empty? lst) ;; empty? is a built-in function to 0 when true - if the list is empty.
+      0
+      (+ 1 (cout_top_level (cdr lst))) ;;if not empty, return 1 + counting the rest of the list - recursively
+  )
+)
+
 ;;D. Write a non-tail recursive function called count_instances that counts the number
 ;;of times an item occurs in a list of items (you may assume all items are atomic).
+(define (count_instances item lst)
+  (cond
+    [(null? lst) 0] ;; Base case - list is empty / finished counting
+    [(equal? (car lst) item) (+ 1 (count_instances item (cdr lst)))] ;;If the first element in list matches return incremented by 1 + recursively count the remainder of the list 
+    [else (count_instances(cdr lst))] ;;Otherwise, continue with the rest of the list.
+  )
+)
+
+
 ;;E. Write a tail-recursive version of your solution to part D called count_instances_tr
+(define (tail_count_instances item lst cnt)
+  (cond
+    [(null? lst) cnt] ;; Base case - list is empty, finished counting, return count
+
+    ;; if first element is equals to the item
+    [(eq? (car lst) item)
+      ;; increment 1 + recurse remainder of the list
+      (tail_count_instances item (cdr lst) (+ cnt 1))
+    ]
+
+    ;; else - first element isn't equal to the item
+  [else 
+    ;; recursively count the remainder of the list - count is the same
+    (tail_count_instances item (cdr lst) cnt)
+  ]
+  )
+)
+
+;; Tail recursive function for count of a given item in a list - assumption all items are atomic
+(define (tail_count_instances_helper item lst)
+  ;;Calling the helper with starter count - 0 
+  (tail_count_instances item lst 0)
+)
+  
+
 ;;F. Write a Scheme function named count_instances_deep that counts the number of
 ;;times an item occurs in a list of items; note that the list may contain sub-lists and you
 ;;should also count occurrences in those lists.
-
-
+(define (count_instances_deep item lst)
+  (cond
+    [(null? lst)  ;; Base case - list is empty / finished counting
+      0
+    ]  
+    
+    ;; If the first element in the list is a list, recurse through the list, and then recurse through the remainder of the main list
+    ;; returning the sum of the two results 
+    [(pair? (car lst))
+      (+ (count_instances_deep item (car lst)) (count_instances_deep item (cdr lst)))
+    ]
+    
+    ;; If the first element in list matches return incremented by 1 + recursively count the remainder of the list 
+    [(equal? (car lst) item)
+      (+ 1 (count_instances_deep item (cdr lst)))
+    ] 
+    
+    [else (count_instances_deep item (cdr lst))] ;;Otherwise, continue with the rest of the list.
+  )
+)
 
 
