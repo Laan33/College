c3a011aef356ddc2f5f6d3b7448af23618c445d1
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-11-09 14:38:58 +0000

    question3 start

---
+++ Year 3/CT331 Programming Paradigms/Assignments/Assignment 2/Assignment2q3.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+
+(provide display_in_order)
+
+
+
+;; A. Display in sorted order the contents of a binary search tree
+
+;; Once the binary search tree has been created, its elements can be retrieved in-order by: 
+  ;; ● Recursively traversing the left subtree of the root node.
+  ;; ● Accessing the node itself
+  ;; ● Then recursively traversing the right subtree of the node
+
+(define display_in_order
+
+
+)
+
+;; B. Return #t or #f if a given item is present or absent in a tree or not. The function
+;; should take the item and a list representing a tree.
+
+
+
+;; C. Insert an item correctly into a list representing a binary search tree. Your function
+;; should take an item and a tree as inputs.
+
+
+
+;; D. Take a list of items and insert them into a binary search tree.
+
+
+;; E. Implement a tree-sort algorithm. Your function should take a list of items and display
+;; them in sorted order.
+
+
+;; F. Implement a higher order version of the tree-sort function that takes a list and a
+;; function that determines the sorted order. For example, write a version that sorts
+;; the list in ascending, descending and ascending based on last digit.
