3f37239f41e18a07d3103cf7d7129ab74ae7cc4f
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-11-11 13:46:59 +0000

    Paradigms - Assignment 3 Prolog - q5

---
+++ Year 3/CT331 Programming Paradigms/Assignments/Assignment 3/Assignmnet3q5.pl
@@ -0,0 +1,30 @@
+
+/*
+Write a set of prolog facts and rules called reverseList that reverses a given list. The
+given list must be the first argument and the reversed list must be the thrird
+Eg: reverseList(List, ReversedList) …
+Your code should produce the following results:
+?- reverseList([1,2,3], X).
+X = [3,2,1].
+?- reverseList([1], X).
+X = [1].
+?- reverseList([], X).
+X = []. (5 marks)
+
+*/
+
+%Base case - reversing an empty list results in an empty list.
+/*
+reverseList([], []).
+
+reverseList( , , Accumulator)
+
+
+reverseList(List, ReversedList) :-
+    reverseList([Head|Tail], [Tail|Head]). */
+
+reverseList(List, Reversed) :- reverseList(List, [], Reversed).
+
+reverseList([], Reversed, Reversed).
+
+reverseList([H|T], Acc, Reversed) :- reverseList(T, [H|Acc], Reversed).
