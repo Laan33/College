ea64bd3264d17dd8160a5560c7c9243033fd41c7
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-11-11 12:02:34 +0000

    Paradigms - Assignment 3 Prolog - q3

---
+++ Year 3/CT331 Programming Paradigms/Assignments/Assignment 3/Assignmnet3q3.pl
@@ -0,0 +1,40 @@
+/*
+Write a set of prolog facts and rules called isNotElementInList that determine if a
+given element is not in a given list. The element must be the first argument and the
+list must be the second.
+Eg: isNotElementInList(El, List) …
+Your code should produce the following results:
+*/
+
+/*
+?- isNotElementInList(1, []).
+True.
+*/
+
+/*
+?- isNotElementInList(1, [1]).
+False.
+*/
+
+/*
+?- isNotElementInList(1, [2]).
+True.
+*/
+
+/*
+?- isNotElementInList(2, [1, 2, 3]).
+False.
+*/
+
+/*
+?- isNotElementInList(7, [1, 2, 9, 4, 5]).
+True. (5 marks)
+*/
+
+
+isNotElementInList(_ , []) :- !. %Base case, no element is in a list if the list is empty
+
+isNotElementInList(El, [H | T]) :- %else, recurse through the remainder of the list
+    El \= H, %Checking if el is not equals to the head of the list
+    isNotElementInList(El, T). %
+
